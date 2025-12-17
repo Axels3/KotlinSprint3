95358ab8359102f8bc52a24531b1a00fe1d7065c
alexey <t9521413290@yandex.ru>
2025-12-17 10:14:59 +0500

    ks-1-1-1 do smth2

--- src/main/kotlin/lesson1_1/lesson1_1_task1.kt
+++ src/main/kotlin/lesson1_1/lesson1_1_task1.kt
@@ -2,5 +2,5 @@ package org.example.lesson1_1
 
 fun main() {
     val orderCount: Int = 75
-    val thankMessage: String = "Благодарим за покупку!"
+    val thankMessage: String = "Благодарим вас за покупку!"
 }
