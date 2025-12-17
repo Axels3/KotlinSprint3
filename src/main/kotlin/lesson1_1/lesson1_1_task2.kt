6412777db43fa91f4a8e961a6a48b7b07df51bad
alexey <t9521413290@yandex.ru>
2025-12-17 10:47:15 +0500

    ks-1-1-2 do smth

---
+++ src/main/kotlin/lesson1_1/lesson1_1_task2.kt
@@ -0,0 +1,37 @@
+package org.example.lesson1_1
+
+fun main() {
+
+//  вариант прямо по тексту задачи
+
+    val orderCount: Int = 75
+    val thankMessage: String = "Thank you for shopping!"
+
+    println(orderCount)
+    println(thankMessage)
+
+    var employeeCount: Int = 2000
+
+    //println(employeeCount)
+
+    employeeCount -= 1
+
+    println(employeeCount)
+
+    /* альтернативный вариант и на мой взгляд более правильный
+
+    val orderCount: Int = 75
+    val thankMessage: String = "Thank you for shopping!"
+    var employeeCount: Int = 2000
+
+    employeeCount -= 1
+
+    println(orderCount)
+    println(thankMessage)
+    println(employeeCount)
+
+    */
+
+
+
+}
